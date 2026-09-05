7617097c33882967be77bac1b3f0c47c628eab96
the-ivo-robotnic <theivorobotnic@gmail.com>
2026-09-05 15:02:39 -0600

    Updated civil alarms lib

--- hanalei/computer/2/startup.lua
+++ hanalei/computer/2/startup.lua
@@ -3,28 +3,20 @@ package.path = package.path .. ";/usr/lib/?";
 package.path = package.path .. ";/usr/lib/?.lua";
 package.path = package.path .. ";/usr/lib/?/init.lua";
 
--- Startup
 
-local mk_turbine = require("mekanism.turbine");
-local turbine = mk_turbine.Turbine:new();
-local utils = require("utils");
-local capacity = turbine:get_energy_max();
-local capacity_fe, capacity_unit = capacity:as_fe(true);
+local civil_alarms = require("civil-alarms");
+local Alarm = civil_alarms.Alarm;
 
-while true do
-    local fill = turbine:get_energy_fill();
-    local fill_fe, fill_unit = fill:as_fe(true);
-    local fill_perc = utils.round_float(turbine.device.getEnergyFilledPercentage() * 100, 1);
-    local rate, unit = turbine:get_energy_production_rate();
+term.clear();
+term.setCursorPos(1, 1);
 
-    term.clear();
-    term.setCursorPos(1, 1);
-    print(
-        "Turbine:\n" ..
-        "\tProducing: " .. utils.round_float(rate), unit .. "\n" ..
-        "\tBuffer:    " .. utils.round_float(fill_fe), fill_unit .. " / " .. capacity_fe, capacity_unit .. "\n" ..
-        "\tBuffer Filled: " .. utils.round_float(fill_perc) .. "%\n"
-    );
+local evac_alarm = Alarm:new("left", "top", "redstone_relay_6");
 
-    sleep(1 / 20);
-end
+evac_alarm:arm(
+    function ()
+        sleep(12);
+        evac_alarm:silence();
+        sleep(12);
+        evac_alarm:disarm();
+    end
+);
--- lib/civil-alarms/init.lua
+++ lib/civil-alarms/init.lua
@@ -1,38 +1,67 @@
-local async = require("/sys/packages/async");
-
 Alarm = {};
 Alarm.__index = Alarm;
 
-function Alarm:new(horn_code, strobe_code)
+function Alarm:new(horn_code, strobe_code, relay)
     self.horn_code = horn_code;
     self.strobe_code = strobe_code;
+    self.horn = false;
+    self.strobe = false;
+
+    if relay ~= nil then
+        self.rs = peripheral.wrap(relay);
+        print("Got relay:", self.rs);
+    else
+        self.rs = redstone;
+    end
     return self;
 end
 
-function Alarm:strobe()
-    async.spawn(self:async_strobe());
+function Alarm:arm(continue_fn)
+    print("Arming strobe and horn!");
+    self.horn = true;
+    self.strobe = true;
+
+    parallel.waitForAll(
+        function()
+            while self.horn do
+                self.rs.setOutput(self.horn_code, true);
+                os.sleep(3);
+                self.rs.setOutput(self.horn_code, false);
+                os.sleep(1);
+            end
+        end,
+        function()
+            local strobe_time = 1;                                -- seconds
+            local strobe_on_time = 0.1;                           -- seconds
+            local strobe_off_time = strobe_time - strobe_on_time; -- seconds
+            local strobe_count = 0;
+
+            while self.strobe do
+                if strobe_count == 3 then
+                    strobe_count = 0;
+                    os.sleep(strobe_time);
+                else
+                    strobe_count = strobe_count + 1;
+                    self.rs.setOutput(self.strobe_code, true);
+                    os.sleep(strobe_on_time);
+                    self.rs.setOutput(self.strobe_code, false);
+                    os.sleep(strobe_off_time);
+                end
+            end
+        end,
+        continue_fn
+    )
 end
 
-function Alarm:async_strobe()
-    print("Flashing strobe:", self.strobe_code);
-    sleep(1);
-    -- while true do
-    -- end
+function Alarm:silence()
+    print("Silencing horn!");
+    self.horn = false;
 end
 
-function Alarm:horn()
-    async.spawn(self:async_horn());
-end
-
-function Alarm:async_horn()
-    while true do
-        print("Sounding horn:", self.horn_code);
-        sleep(1);
-    end
-end
-
-function Alarm:run_async()
-    async.drive();
+function Alarm:disarm()
+    print("Silencing strobe and horn!");
+    self.horn = false;
+    self.strobe = false;
 end
 
 return { Alarm = Alarm };
